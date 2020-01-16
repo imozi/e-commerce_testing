6a1cd285bf9e5a73b17f3c37114121f033ebb51f
Oleg Zaytsev <zaytsevoi.90@gmail.com>
2020-01-16 18:39:14 +1200

    added style footer & how-we-work section

---
+++ build/js/module/how-we-work.js
@@ -0,0 +1,12 @@
+const howWeWorkList = document.querySelector('.how-we-work__list');
+
+if (howWeWorkList) {
+  return
+}
+
+
+const activeItem = (evt) => {
+  const element = evt.target;
+
+  if (element.)
+}
